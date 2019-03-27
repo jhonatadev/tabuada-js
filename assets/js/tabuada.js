de45901f8391b76c88c9e587100db3c8665c9210
jhonatadev <xxxonata@gmail.com>
2019-03-27 17:04:42 -0300

    trabalhe por mim [Função]

--- assets/js/tabuada.js
+++ assets/js/tabuada.js
@@ -1,23 +1,33 @@
-let tabuada = document.querySelector("#tabuada tbody");
-let valorA = 1;
+function calculaTabuada(){
 
-tabuada.innerHTML = '';
-
-for (let valorB = 0; valorB <= 10; valorB++) {
+    let tabuada = document.querySelector("#tabuada tbody");
+    let valorA = parseInt(document.querySelector("#valorA").value);
     
-    let resultado = valorA * valorB;
+    tabuada.innerHTML = '';
     
-    let template = `
-            <td>${valorA}</td>
-            <td>x</td>
-            <td>${valorB}</td>
-            <td>=</td>
-             <td>${resultado}</td>
-         `;
-
-    let tr = document.createElement('tr');
-
-    tr.innerHTML = template;
-
-    tabuada.append(tr);
+    for (let valorB = 0; valorB <= 10; valorB++) {
+        
+        let resultado = valorA * valorB;
+        
+        let template = `
+                <td>${valorA}</td>
+                <td>x</td>
+                <td>${valorB}</td>
+                <td>=</td>
+                <td>${resultado}</td>
+             `;
+    
+        let tr = document.createElement('tr');
+    
+        tr.innerHTML = template;
+    
+        tabuada.append(tr);
+    }
 }
+
+calculaTabuada();
+
+document.querySelector("#valorA").addEventListener('change', e => {
+    calculaTabuada();
+})
+
